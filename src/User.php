1fbe078bede5ec5bfa57fd4e9d90e2be0eef8ac6
lujxio <jcdiaz92@gmail.com>
2015-08-31 15:14:04 -0700

    adds update functions to User class

--- src/User.php
+++ src/User.php
@@ -84,5 +84,39 @@
 			$GLOBALS['DB']->exec("INSERT INTO users (user_name, password, lng, lat, signed_in) VALUES ('{$this->getUserName()}', '{$this->getPassword()}', {$this->getLongitude()}, {$this->getLatitude()}, {$this->getSignedIn()});");
 			$this->setId($GLOBALS['DB']->lastInsertId());
 		}
+		
+		function updateLocation($new_longitude, $new_latitude)
+		{
+			$GLOBALS['DB']->exec("UPDATE users SET lng = {$new_longitude} WHERE id = {$this->getId()};");
+			$GLOBALS['DB']->exec("UPDATE users SET lat = {$new_latitude} WHERE id = {$this->getId()};");
+			$this->setLongitude($new_longitude);
+			$this->setLatitude($new_latitude);
+		}
+		
+		function updatePassword($new_password)
+		{
+			$GLOBALS['DB']->exec("UPDATE users SET password = '{$new_password}' WHERE id = {$this->getId()};");
+			$this->setPassword($new_password);
+		}
+		
+		function updateUserName($new_user_name)
+		{
+			$GLOBALS['DB']->exec("UPDATE users SET user_name = '{$new_user_name}' WHERE id = {$this->getId()};");
+			$this->setUserName($new_user);
+		}
+		
+		function updateSignedIn($new_signed_in)
+		{
+			$GLOBALS['DB']->exec("UPDATE users SET signed_in = {$new_signed_in} WHERE id = {$this->getId()};");
+			$this->setSignedIn($new_signed_in);
+		}
+		
+		function update($new_user_name, $new_password, $new_longitude, $new_latitude, $new_signed_in)
+		{
+			$this->updateUserName($new_user_name);
+			$this->updatePassword($new_password);
+			$this->updateLocation($new_longitude, $new_latitude);
+			$this->update($new_signed_in);
+		}
 	}
 ?>
